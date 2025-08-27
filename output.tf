6d4b58bdb249e18294dfec0636f1a464233d74ce
Ajo Panoor <a.josepanoor@celonis.com>
2025-08-27 13:30:03 +0200

    fixes

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 output "private_endpoint_name" {
   description = "The name of the Private Link Endpoint"
-  value       = azurerm_private_endpoint.priv_endpoint.name
+  value       = azurerm_private_endpoint.priv_endpoint[each.key].name
 }
 
 output "private_endpoint_ip" {
   description = "The private IP address of the Private Link Endpoint"
-  value       = azurerm_private_endpoint.priv_endpoint.private_service_connection[0].private_ip_address
+  value       = azurerm_private_endpoint.priv_endpoint.private_service_connection[each.key].private_ip_address
 }
 
 output "private_endpoint_id" {
   description = "The ID of the Private Link Endpoint"
-  value       = azurerm_private_endpoint.priv_endpoint.id
+  value       = azurerm_private_endpoint.priv_endpoint[each.key].id
 }
