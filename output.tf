44e2b21cc1185a5bf4dc964f3ade1f370b739841
Ajo Panoor <a.josepanoor@celonis.com>
2025-08-27 13:45:57 +0200

    fixes

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "private_endpoint_ip" {
   description = "The private IP address of the Private Link Endpoint"
-  value       = { for k, endpoint in azurerm_private_endpoint.priv_endpoint : k => endpoint.private_ip_address }
+  value       = { for k, endpoint in azurerm_private_endpoint.priv_endpoint : k => endpoint.private_service_connection[0].private_ip_address }
 }
