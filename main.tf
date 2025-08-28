f8290256648233a6b7328ce9155cfff2bdb43dee
Ajo Panoor <a.josepanoor@celonis.com>
2025-08-28 15:09:53 +0200

    fixes

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link" {
   resource_group_name   = var.resource_group_name
   private_dns_zone_name = azurerm_private_dns_zone.dns_zone[each.key].name
   virtual_network_id    = var.vnet_id
-  registration_enabled  = true
+  registration_enabled  = false
   tags                  = var.tags
 }
 
